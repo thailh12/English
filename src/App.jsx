779498b1ebab50d7f93dada62b58993b8f44787f
thaiha154 <thaiha15499@gmail.com>
2017-11-09 09:06:15 +0700

    add antonym

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,62 @@
 import React, {Component} from 'react';
 
-
 class App extends Component {
-  show(json){
+  show(json) {
     if (json.hasOwnProperty('adjective')) {
-      this.setState({description: json.adjective.syn + " "})
+      this.setState({
+        synonym: json.adjective.syn + " "
+      })
+      this.setState({
+        antonym: json.adjective.ant + " "
+      })
     } else if (json.hasOwnProperty('noun')) {
-      this.setState({description: json.noun.syn+" "})
+      this.setState({
+        synonym: json.noun.syn + " "
+      })
     } else if (json.hasOwnProperty('verb')) {
-      this.setState({description: json.verd.syn +" "})
-    } else {this.setState({description:"404 Not Found"})}
+      this.setState({
+        synonym: json.verd.syn + " "
+      })
+    } else {
+      this.setState({description: "404 Not Found"})
+    }
     console.log(this.state);
   }
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      word:"",
-      description:""
+    this.state = {
+      word: "",
+      synonym: "",
+      antonym: ""
     }
   }
 
-  call(word){
-    fetch(`http://words.bighugelabs.com/api/2/53ffd46ebac03632a4179401a3cae48b/${word}&/json`)
-    .then(response => response.json())
-    .then(json => {this.show(json)
-      })
-    }
+  call(word) {
+    this.setState({word: "", synonym: "", antonym: ""});
 
-  render(){
-    return(
-      <div className="form-group">
-        <input
-          id="input"
-          type="text"
-          placeholder="your word..."
-          onChange={event => this.setState({word: event.target.value})}
-        />
-        <button className="btn btn-success"
-          type="button"
-          onClick={() =>this.call(this.state.word)}
-          >
-          submit
-        </button>
-        <div>
-          <h1>Synonym of {this.state.word} is</h1><h2>{this.state.description}</h2>
-        </div>
+    fetch(`http://words.bighugelabs.com/api/2/53ffd46ebac03632a4179401a3cae48b/${word}&/json`).then(response => response.json()).then(json => {
+      this.show(json)
+    })
+  }
+
+  render() {
+    return (<div className="form-group">
+      <input id="input" type="text" placeholder="your word..." onChange={event => this.setState({word: event.target.value})}/>
+      <button className="btn btn-success" type="button" onClick={() => this.call(this.state.word)}>
+        submit
+      </button>
+      <div className="output">
+        <h1>Synonym of {this.state.word}
+          is</h1>
+        <h2>{this.state.synonym}</h2>
       </div>
-    )
+      <div>
+        <h1>Antonym of {this.state.word}
+          is</h1>
+        <h2>{this.state.antonym}</h2>
+      </div>
+    </div>)
   }
 }
 
