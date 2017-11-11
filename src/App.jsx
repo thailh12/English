d8d9021936776266c15d1cc9ac634f16edf69f72
thaiha154 <thaiha15499@gmail.com>
2017-11-11 21:11:07 +0700

    run with 1 word, sometimes crash when don't have antonym or synonym

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,23 @@ class App extends Component {
   first(json) {
     this.setState({synonym: [
       json[0].word,
-      json[1].word,
-      json[2].word,
-      json[3].word,
-      json[4].word
+      //json[1].word,
+    //  json[2].word,
+  //    json[3].word,
+  //    json[4].word
     ]});
   }
   second(json){
     this.setState({antonym: [
-      json[0].word,
-      json[1].word,
-      json[2].word,
-      json[3].word,
-      json[4].word
+      json[0].word
+      //json[1].word,
+    // json[2].word,
+  //    json[3].word,
+    //  json[4].word
     ]});
 
-  }
-
+//  }
+}
   constructor(props) {
     super(props);
     this.state = {
@@ -56,32 +56,26 @@ class App extends Component {
           <input className="form-control" type="text" placeholder="your word..." onChange={event => this.setState({word: event.target.value})}/>
           <button className="btn btn-success" type="button" onClick={() => {
             this.call2(this.state.word)
-            this.call1(this.state.word)}}>
+            this.call1(this.state.word)
+          }}>
             SUBMIT
           </button>
         </div>
       </div>
       <div className="output">
         <div className="syn">
-          <h1>Synonym of {this.state.word} is</h1>
+          <h1>Synonym of {this.state.word} </h1>
           <h2 id="out">{this.state.synonym[0]}</h2>
           <h2 id="out">{this.state.synonym[1]}</h2>
-          <h2 id="out">{this.state.synonym[2]}</h2>
-          <h2 id="out">{this.state.synonym[3]}</h2>
-          <h2 id="out">{this.state.synonym[4]}</h2>
-
         </div>
         <div className="ant">
-          <h1>Antonym of {this.state.word} is</h1>
+          <h1>Antonym of {this.state.word} </h1>
           <h2 id="out">{this.state.antonym[0]}</h2>
           <h2 id="out">{this.state.antonym[1]}</h2>
-          <h2 id="out">{this.state.antonym[2]}</h2>
-          <h2 id="out">{this.state.antonym[3]}</h2>
-          <h2 id="out">{this.state.antonym[4]}</h2>
         </div>
       </div>
       <div className="footer">
-        <span>Power by React <br /> Develop by Thai Lam Ha <br /> Design by jiichu</span>
+        <span>Power by datamuse API <br /> Develop by Thai Lam Ha <br /> Design by jiichu</span>
       </div>
     </div>)
   }
